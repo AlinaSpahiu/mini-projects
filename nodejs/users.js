15d36bb7f3955d4451cb8fd41a45265cc8eb03cf
AlinaSpahiu <alina.s.abbaz@gmail.com>
2021-01-06 15:35:19 +0100

    Create users.js

---
+++ nodejs/users.js
@@ -0,0 +1,16 @@
+const users = [
+    {
+        "id": "1",
+        "name": "Alina",
+        "surname": "spahiu",
+    },
+
+    {
+        "id": "2",
+        "name": "Hasan",
+        "surname": "Abbz",
+    }
+]
+
+const nums = [1, 2, 3]
+module.exports = { users, nums }
