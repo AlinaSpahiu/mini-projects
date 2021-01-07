b824c0cc298834ba87ca1d5155cb00bd8cdb9e40
AlinaSpahiu <alina.s.abbaz@gmail.com>
2021-01-07 14:23:49 +0100

    Update dogImage.js

--- ajax/examples/src/components/dogImage.js
+++ ajax/examples/src/components/dogImage.js
@@ -1,8 +1,8 @@
-import React, { Component } from "react";
+// import React, { Component } from "react";
 
-function dogImage(props) {
-    return (
-        <img src={props.url} />
-    )
-}
-export default dogImage
+// function dogImage(props) {
+//     return (
+//         <img src={props.url} />
+//     )
+// }
+// export default dogImage
