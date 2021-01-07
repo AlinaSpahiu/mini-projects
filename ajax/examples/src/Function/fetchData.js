60a24eb899fde53969d8631966b51fafe3ab849c
AlinaSpahiu <alina.s.abbaz@gmail.com>
2021-01-07 14:23:58 +0100

    Update fetchData.js

--- ajax/examples/src/Function/fetchData.js
+++ ajax/examples/src/Function/fetchData.js
@@ -1,33 +1,33 @@
 
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+// import React, { useState, useEffect } from "react";
+// import axios from "axios";
 
 
-function App() {
-    const [piadas, setPiadas] = useState([]);
+// function App() {
+//     const [piadas, setPiadas] = useState([]);
 
-    useEffect(() => {
-        (async () => {
-            const res = await axios.get(
-                "https://official-joke-api.appspot.com/jokes/programming/ten"
-            );
-            setPiadas(res.data);
-        })();
-    }, []);
+//     useEffect(() => {
+//         (async () => {
+//             const res = await axios.get(
+//                 "https://official-joke-api.appspot.com/jokes/programming/ten"
+//             );
+//             setPiadas(res.data);
+//         })();
+//     }, []);
 
-    return (
-        <div className="App">
-            <ul>
-                {piadas.map((piada, index) => {
-                    return (
-                        <li key={index}>
-                            {piada.setup} {piada.punchline}
-                        </li>
-                    );
-                })}
-            </ul>
-        </div>
-    );
-}
+//     return (
+//         <div className="App">
+//             <ul>
+//                 {piadas.map((piada, index) => {
+//                     return (
+//                         <li key={index}>
+//                             {piada.setup} {piada.punchline}
+//                         </li>
+//                     );
+//                 })}
+//             </ul>
+//         </div>
+//     );
+// }
 
-export default App;
+// export default App;
